2506d264bafca2bdf29fecad82e763833e29b76b
imkenova <54978866+imkenova@users.noreply.github.com>
2021-10-14 23:04:50 +0300

    Update header

--- src/Header.js
+++ src/Header.js
@@ -14,10 +14,10 @@ function Header() {
         <Slide top>
             <div className="row valign-wrapper">
                 <div className="col s12 m12 l12 hide-on-med-and-up center-align">
-                    <img src="logo.png"/> <span className="logo-text blue-text">Заметки</span>
+                    <img src="logo.png"/> <span className="logo-text blue-text">ToDo App</span>
                 </div>
                 <div className="col s12 m6 l6 left-align grey-text hide-on-small-only">
-                    <img src="logo.png"/> <span className="logo-text blue-text">Заметки</span>
+                    <img src="logo.png"/> <span className="logo-text blue-text">ToDo App</span>
                 </div>
                 <div className="col s12 m6 l6 right-align hide-on-small-only grey-text">
                     {userNotification}
