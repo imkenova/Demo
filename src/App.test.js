8419d26e632c7302b885ff05e538133e1ad51254
imkenova <54978866+imkenova@users.noreply.github.com>
2021-10-15 20:41:45 +0300

    update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders text on the start screen', () => {
-  render(<App />);
-  expect(screen.getByText('Выберите категорию')).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
