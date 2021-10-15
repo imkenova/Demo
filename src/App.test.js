d0cfefa20f33394ca44d03359427c37a6237f877
imkenova <54978866+imkenova@users.noreply.github.com>
2021-10-15 20:08:15 +0300

    test text

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders text on the start screen', () => {
+  render(<App />);
+  expect(screen.getByText('Выберите категорию')).toBeInTheDocument();
 });
